db008fc3ac2a49c2af3d20a3297f4cf2fc6b2c3e
Montana Mendy <montana@linux.com>
2022-08-05 12:40:41 -0700

    fix bash

--- step.sh
+++ step.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "testing" | sudo step certificate create --profile root-ca "Montana Mendy Travis CI keys" ca.crt ca.key
+pass="testing"
+echo $pass | step certificate create --profile root-ca "Montana Mendy Travis CI keys" ca.crt ca.key
