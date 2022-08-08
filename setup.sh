deab934de38c7dd0a406c2b9426ffe27db1a3112
Montana Mendy <montana@linux.com>
2022-08-08 09:49:48 -0700

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://dl.step.sm/gh-release/cli/docs-cli-install/v0.20.0/step-cli_0.20.0_amd64.deb
+sudo dpkg -i step-cli_0.20.0_amd64.deb
+step -v 
+step certificate create --profile root-ca "Montana Mendy Travis CI keys" ca.crt ca.key --insecure --no-password
+step certificate inspect https://smallstep.co
